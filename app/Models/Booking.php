3a1e9273fa64fe0e3674c33c6671f97bca348b1c
Geo623 <112695394+Geo623@users.noreply.github.com>
2025-01-08 23:48:53 +0200

    Update Booking.php

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -17,6 +17,10 @@ class Booking extends Model
     {
         return $this->belongsTo(Vehicle::class);
     }
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function scopeAvailable($query, $vehicleId, $startDate, $endDate)
     {
